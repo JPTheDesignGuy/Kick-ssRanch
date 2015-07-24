846d5d3a7aef59acb9336d05ae291fdad17c600a
JPTheDesignGuy <mac@jpthedesignguy.com>
2015-07-24 19:36:07 -0400

    Committing to Master
    This is the first push of Kick Ass Ranch

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+/* ==============================================
+ WOW plugin triggers animate.css on scroll
+ =============================================== */
+
+var wow = new WOW(
+        {
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 100, // distance to the element when triggering the animation (default is 0)
+            mobile: false        // trigger animations on mobile devices (true is default)
+        }
+);
+wow.init();
+
+
+
+
+
+
+//smooth scroll
+$(function() {
+	  $('.scrollto a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top - 50
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});	
+        
+        
+
+
+//back to top
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 800) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
